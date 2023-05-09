c55e90b86a3a1454be4983612056cdc05dd7d16a
SohnEugene <102849934+SohnEugene@users.noreply.github.com>
2023-05-09 11:32:00 +0900

    sign in

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -24,10 +24,8 @@ const TodoItem = ({ todo, onToggle, onDelete }) => {
       >
         {todo.text}
       </span>
-      <span
-        className= "justify-self-end p-1 mr-4"
-        style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
-        {todo.dateTemp}
+      <span className={styles.todoDatetime}>
+        {todo.datetime.substring(0, 10)}
       </span>
       {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
       <button className="w-30 justify-self-end p-1 bg-rose-300 text-white border border-rose-300 rounded hover:bg-white hover:text-rose-500" 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,21 +4,27 @@
   할 일 목록의 추가, 삭제, 완료 상태 변경 등의 기능을 구현하였습니다.
 */
 import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
+
 import TodoItem from "@/components/TodoItem";
 import styles from "@/styles/TodoList.module.css";
 
+// firebase 관련 모듈을 불러옵니다.
 import { db } from "@/firebase";
 import {
   collection,
   query,
   doc,
-  getDocs, 
+  getDocs,
   addDoc,
-  updateDoc, 
+  updateDoc,
   deleteDoc,
   orderBy,
+  where,
 } from "firebase/firestore";
 
+// DB의 todos 컬렉션 참조를 만듭니다. 컬렉션 사용시 잘못된 컬렉션 이름 사용을 방지합니다.
 const todoCollection = collection(db, "todos");
 
 // TodoList 컴포넌트를 정의합니다.
@@ -27,22 +33,38 @@ const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
 
-  const getTodos = async() => {
-    const q = query(todoCollection);
+  const { data } = useSession();
+
+  const getTodos = async () => {
+    // Firestore 쿼리를 만듭니다.
+    // const q = query(todoCollection);
+    // const q = query(collection(db, "todos"), where("user", "==", user.uid));
+    // const q = query(todoCollection, orderBy("datetime", "asc"));
+    if (!data?.user?.name) return;
+
+    const q = query(
+      todoCollection,
+      where("userId", "==", data?.user?.id),
+      orderBy("datetime", "asc")
+    );
+
+    // Firestore 에서 할 일 목록을 조회합니다.
     const results = await getDocs(q);
     const newTodos = [];
 
+    // 가져온 할 일 목록을 newTodos 배열에 담습니다.
     results.docs.forEach((doc) => {
-      newTodos.push({id:doc.id, ...doc.data()});
+      // console.log(doc.data());
+      // id 값을 Firestore 에 저장한 값으로 지정하고, 나머지 데이터를 newTodos 배열에 담습니다.
+      newTodos.push({ id: doc.id, ...doc.data() });
     });
 
-    newTodos.sort((a, b) => a.timeStamp - b.timeStamp);
     setTodos(newTodos);
   };
 
   useEffect(() => {
     getTodos();
-  }, []);
+  }, [data]);
 
   // addTodo 함수는 입력값을 이용하여 새로운 할 일을 목록에 추가하는 함수입니다.
   const addTodo = async () => {
@@ -56,18 +78,21 @@ const TodoList = () => {
     // }
     // ...todos => {id: 1, text: "할일1", completed: false}, {id: 2, text: "할일2", completed: false}}, ..
 
-    const date = new Date();
-    const timeStamp = Date.now();
-    const dateString = date.getFullYear() + "/" + (date.getMonth()+1) + "/" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes().toString().padStart(2, '0');;
+    const datetime = new Date().toISOString();
 
+    // Firestore 에 추가한 할 일을 저장합니다.
     const docRef = await addDoc(todoCollection, {
+      userId: data?.user?.id,
       text: input,
       completed: false,
-      dateTemp: dateString,
-      timeStamp: timeStamp
+      datetime: datetime,
     });
-    
-    setTodos([...todos, { id: timeStamp, text: input, completed: false, dateTemp: dateString, timeStamp: timeStamp}]);
+
+    // id 값을 Firestore 에 저장한 값으로 지정합니다.
+    setTodos([
+      ...todos,
+      { id: docRef.id, text: input, datetime: datetime, completed: false },
+    ]);
     setInput("");
   };
 
@@ -76,9 +101,11 @@ const TodoList = () => {
     // 할 일 목록에서 해당 id를 가진 할 일의 완료 상태를 반전시킵니다.
     const newTodos = todos.map((todo) => {
       if (todo.id === id) {
+        // Firestore 에서 해당 id를 가진 할 일을 찾아 완료 상태를 업데이트합니다.
         const todoDoc = doc(todoCollection, id);
-        updateDoc(todoDoc, {completed: !todo.completed});
-        return {...todo, completed: !todo.completed};
+        updateDoc(todoDoc, { completed: !todo.completed });
+        // ...todo => id: 1, text: "할일1", completed: false
+        return { ...todo, completed: !todo.completed };
       } else {
         return todo;
       }
@@ -89,11 +116,12 @@ const TodoList = () => {
 
   // deleteTodo 함수는 할 일을 목록에서 삭제하는 함수입니다.
   const deleteTodo = (id) => {
-    // 해당 id를 가진 할 일을 제외한 나머지 목록을 새로운 상태로 저장합니다.
-    // setTodos(todos.filter((todo) => todo.id !== id));
+    // Firestore 에서 해당 id를 가진 할 일을 삭제합니다.
     const todoDoc = doc(todoCollection, id);
     deleteDoc(todoDoc);
 
+    // 해당 id를 가진 할 일을 제외한 나머지 목록을 새로운 상태로 저장합니다.
+    // setTodos(todos.filter((todo) => todo.id !== id));
     setTodos(
       todos.filter((todo) => {
         return todo.id !== id;
@@ -104,8 +132,19 @@ const TodoList = () => {
   // 컴포넌트를 렌더링합니다.
   return (
     <div className={styles.container}>
+      <button
+            className={`w-40
+                      justify-self-center
+                      p-1 mb-4
+                    text-rose-500
+                      border border-rose-500 rounded
+                    hover:bg-white hover:text-rose-500`}
+            onClick={() => signOut()}
+          >
+            Sign out
+          </button>
       <h1 className="text-xl mb-4 font-bold underline underline-offset-4 decoration-wavy">
-        Todo List
+        {data?.user?.name}'s Todo List
       </h1>
       {/* 할 일을 입력받는 텍스트 필드입니다. */}
       <input
@@ -117,12 +156,12 @@ const TodoList = () => {
         //   padding: 5px;
         //   margin-bottom: 10px;
         // }
-        className="shadow-lg w-full p-1 mb-4 border border-gray-300 rounded"
+        className="w-full p-1 mb-4 border border-gray-300 rounded"
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
       {/* 할 일을 추가하는 버튼입니다. */}
-      <div class="grid">
+      <div className="grid">
         <button
           // className={styles.addButton}
           // -- addButton CSS code --
@@ -139,7 +178,12 @@ const TodoList = () => {
           //   background-color: #fff;
           //   color: #0070f3;
           // }
-          className="shadow-lg w-40 justify-self-end p-1 mb-4 bg-rose-400 text-white border border-rose-400 rounded hover:bg-white hover:text-rose-500"
+          className={`w-40
+                      justify-self-end
+                      p-1 mb-4
+                    bg-rose-500 text-white
+                      border border-rose-500 rounded
+                    hover:bg-white hover:text-rose-500`}
           onClick={addTodo}
         >
           Add Todo
