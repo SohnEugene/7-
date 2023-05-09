b2706b0fa145c18f5b6762cd7a458059455aaaa7
SohnEugene <102849934+SohnEugene@users.noreply.github.com>
2023-05-09 15:13:30 +0900

    firebase key 환경변수로

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCv72WcR7O3hDkjL5Ogcr1L-WV6RYx235o",
-  authDomain: "fir-practice-d38ef.firebaseapp.com",
-  projectId: "fir-practice-d38ef",
-  storageBucket: "fir-practice-d38ef.appspot.com",
-  messagingSenderId: "259236433070",
-  appId: "1:259236433070:web:2a03e2105a2b8149b8313f",
-  measurementId: "G-4D8QSS1HBN"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
